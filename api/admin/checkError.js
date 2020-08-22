fc9daeb3b98f66a3a1efc9c5ee8da9a90611c47a
lynxgsm <aili.fida.ac@gmail.com>
2020-08-22 15:32:08 +0200

    package.json

--- api/admin/checkError.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  isValid(obj) {
-    const tmp = Object.keys(obj);
-    return { error: obj[tmp[0]].message };
-  },
-};
--- index.js
+++ index.js
@@ -1,64 +1,10 @@
-var cors = require("cors");
-var http = require("http");
-var dotenv = require("dotenv");
-var express = require("express");
-var socket = require("socket.io");
-var bodyParser = require("body-parser");
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-dotenv.config();
-
-var DBCONNECT = require("./config/db");
-var { HTTP } = require("./config/setting").config;
-var auth = require("./api/middlewares/authorization");
-
-//init app
-var app = express();
-var httpServer = http.createServer(app);
-
-var ioHttp = socket(httpServer);
-var Router = express.Router();
-
-DBCONNECT();
-
-//app config
-app.use(cors());
-app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-app.use((req, res, next) => {
-  req.ioHttp = ioHttp;
-  next();
-});
-
-app.get("/", (req, res) => {
-  return res.send("Hello from server");
-});
-
-app.use("/api/admin/*", auth(0));
-app.use("/api/client/*", auth(1));
-app.use("/api/resto/*", auth(2));
-app.use("/api/livreur/*", auth(3));
-app.use("/api/supermarche/*", auth(4));
-app.use("/api/marchand/*", auth(5));
-app.use("/api/common/*", auth("*"));
-
-app.use("/api", require("./api/common/controllers")(Router));
-app.use("/api", require("./api/supermarche")(Router));
-app.use("/api", require("./api/restaurant")(Router));
-app.use("/api", require("./api/marchand")(Router));
-app.use("/api", require("./api/livreur")(Router));
-app.use("/api", require("./api/client")(Router));
-app.use("/api", require("./api/admin")(Router));
-
-ioHttp.on("connection", function (socket) {
-  console.log("Socket.io connected");
-});
-
-httpServer.listen(HTTP, () => {
-  console.log(`Server started on http://${HTTP.host}:${HTTP.port}`);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
