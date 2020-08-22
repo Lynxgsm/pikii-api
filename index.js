0ad834591f4c8ea0dfff50954e10022c1f9ca1ef
lynxgsm <aili.fida.ac@gmail.com>
2020-08-22 16:02:19 +0200

    package.json

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const path = require('path')
 const socket = require("socket.io");
 const PORT = process.env.PORT || 5000
+const cors = require("cors");
+const http = require("http");
 
 express()
-  .get('/', (req, res) => res.send('Hello World'))
+  .get('/', (req, res) => res.send('Hello World with cors and http'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
