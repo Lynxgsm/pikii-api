94e40a665d7736f6b485fe97973d7d23d9a48a20
lynxgsm <aili.fida.ac@gmail.com>
2020-08-22 16:08:03 +0200

    package.json

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const path = require('path')
-const socket = require("socket.io");
-const PORT = process.env.PORT || 5000
 const cors = require("cors");
 const http = require("http");
+const dotenv = require("dotenv");
+const express = require("express");
+const socket = require("socket.io");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000
 
 express()
   .get('/', (req, res) => res.send('Hello World with cors and http'))
