ee2c5e9fd74cb8191d4aa8a7ef07915356af61b7
lynxgsm <aili.fida.ac@gmail.com>
2020-08-22 15:55:10 +0200

    package.json

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// var cors = require("cors");
-// var http = require("http");
-// var dotenv = require("dotenv");
 var express = require("express");
-// var socket = require("socket.io");
-// var bodyParser = require("body-parser");
-
-// dotenv.config();
-
-// var DBCONNECT = require("./config/db");
-// var { HTTP } = require("./config/setting").config;
 
 express()
   .get('/', (req, res) => res.send('helloworld'))
